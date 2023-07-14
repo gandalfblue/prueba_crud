27c08b42bba72180e1b210c1e499618031d51d27
gandalfblue <casanova01_ahotmail.com>
2023-07-14 18:20:38 -0500

    feature/implementation of the constant variables

---
+++ src/main/java/com/prueba/crud/infrastructure/shared/constants/RestConstants.java
@@ -0,0 +1,8 @@
+package com.prueba.crud.infrastructure.shared.constants;
+
+public class RestConstants {
+
+	public static final String APPLICATION_NAME = "crud_prueba";
+	public static final String API_VERSION_1 = "/v1";
+	public static final String RESOURCE_USER = "/user";
+}
