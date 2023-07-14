49051aba40a07bb5a83abaf3610e31bf95cd5469
gandalfblue <casanova01_ahotmail.com>
2023-07-14 18:23:01 -0500

    feature/implementation of the configurations of webflux and beans

---
+++ src/main/java/com/prueba/crud/infrastructure/configuration/WebFluxConfig.java
@@ -0,0 +1,48 @@
+package com.prueba.crud.infrastructure.configuration;
+
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
+
+/**
+ * Clase que sirve para realizar la configuración de los accesos a los servicios REST
+ */
+@Configuration
+public class WebFluxConfig {
+
+    @Bean
+    public WebFluxConfigurer corsConfigure() {
+        return new WebFluxConfigurerComposite() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("*");
+            }
+        };
+    }
+
+    @Bean
+    public WebProperties.Resources resources() {
+        return new WebProperties.Resources();
+    }
+
+    /*@Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }*/
+
+    /*@Bean
+    public LocalValidatorFactoryBean validatorFactoryBean(MessageSource messageSource) {
+        LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+        localValidatorFactoryBean.setValidationMessageSource(messageSource);
+        return localValidatorFactoryBean;
+    }*/
+}
